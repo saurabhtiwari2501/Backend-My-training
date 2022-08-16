9365904a01bae4fb44e09a0e8ced99b2d9d8f520
saurabhtiwari2501 <saurabhtiwari2501@gmail.com>
2022-08-16 11:37:55 +0530

    Node get  api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+var app = express();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -34,5 +35,53 @@ router.get('/student-details/:name', function(req, res){
     
     res.send('Dummy response')
 })
+
+
+ // -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+
+
+ //Logic start from here
+
+
+app.get("/Array-01", function (req, res) {
+
+    let arr= [1,2,3,5,6,7]
+ 
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+ 
+   let lastDigit= arr.pop()
+   let consecutiveSum= lastDigit * (lastDigit+1) /2
+   let missingNumber= consecutiveSum - total
+ 
+   res.send(  { data: missingNumber  }  );
+ });
+
+
+
+
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+app.get("/sol2", function (req, res) {
+
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+ 
 
 module.exports = router;
