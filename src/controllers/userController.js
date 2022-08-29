b30bc1fd6fbd11cf348d08545742ec7b7d9dd247
saurabhtiwari2501 <saurabhtiwari2501@gmail.com>
2022-08-29 22:56:48 +0530

    assignment auth-1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,7 +22,7 @@ const loginUser = async function (req, res) {
   if (!user)
     return res.send({
       status: false,
-      msg: "username or the password is not corerct",
+      msg: "user name or the password is not corerct",
     });
 
   // Once the login is successful, create the jwt token with sign function
@@ -37,7 +37,7 @@ const loginUser = async function (req, res) {
       batch: "Plutonium",
       organisation: "FunctionUp",
     },
-    "functionup-plutonium-saurabh-very-very-secret-key"
+    "saurabh"
   );
   res.setHeader("x-auth-token", token);
   res.send({ status: true, token: token });
@@ -51,17 +51,9 @@ const getUserData = async function (req, res) {
   if (!token) return res.send({ status: false, msg: "token must be present" });
 
   console.log(token);
+  let decodedToken = jwt.verify(token, "saurabh");
+  //console.log(decodedToken); //
 
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-
-  // Decoding requires the secret again. 
-  // A token can only be decoded successfully if the same secret was used to create(sign) that token.
-  // And because this token is only known to the server, it can be assumed that if a token is decoded at server then this token must have been issued by the same server in past.
-  let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
   if (!decodedToken)
     return res.send({ status: false, msg: "token is invalid" });
 
